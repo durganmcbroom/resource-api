8486935b085c6905fcca75456d143626754dd390
durganmcbroom <durganmcbroom@gmail.com>
2025-06-09 07:30:20 -0700

    Removed socket timeouts.

--- src/commonMain/kotlin/com/durganmcbroom/resources/KtorInstance.kt
+++ src/commonMain/kotlin/com/durganmcbroom/resources/KtorInstance.kt
@@ -2,23 +2,9 @@ package com.durganmcbroom.resources
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.*
 
 public object KtorInstance {
     public val client: HttpClient = HttpClient(CIO) {
-        engine {
-            maxConnectionsCount = 100
-            endpoint {
-                connectTimeout = 60_000
-                socketTimeout = 60_000
-            }
-        }
-        install(HttpTimeout) {
-            requestTimeoutMillis = 60_000
-            connectTimeoutMillis = 60_000
-            socketTimeoutMillis = 60_000
-        }
-
         // Enable automatic following of redirects.
         followRedirects = true
     }
